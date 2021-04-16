48d836faf6d7246bb7209a2948140476e18bf904
KoreyEnglert <englertk@oregonstate.edu>
2021-04-15 17:09:05 -0700

    fixed the funciton and added more tests

--- divisors.py
+++ divisors.py
@@ -1,6 +1,10 @@
 def calc(a): #commit 1
-    for x in range((int)(a/2) + 1):
+    for x in range(a + 1):
         if(x !=0 and a % x == 0):
             print(x)
 
 calc(60)
+print(" ")
+calc(20)
+print(" ")
+calc(9)

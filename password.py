ab8bb527b85be20b0f9a7064f3a362226c133362
KoreyEnglert <englertk@oregonstate.edu>
2021-04-15 17:15:21 -0700

    untested rough draft

---
+++ password.py
@@ -0,0 +1,18 @@
+import random
+
+def password(a): #commit 1
+    list [];
+    for x in range(a):
+        a = random.randint(0, 61) + 48;
+        if(a > 57):
+            a += 8
+        if(a > 90):
+            a += 7
+            list.append(a)
+    print(list);
+
+password(60)
+print(" ")
+password(20)
+print(" ")
+password(9)

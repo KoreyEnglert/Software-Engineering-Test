e96a24649569e4356bbd82b0ed0000fa850f55bf
KoreyEnglert <englertk@oregonstate.edu>
2021-04-15 17:26:37 -0700

    fixed the random character generation

--- password.py
+++ password.py
@@ -1,18 +1,20 @@
 import random
 
 def password(a): #commit 1
-    list = [];
+    p = "";
+    d = "";
     for x in range(a):
-        a = random.randint(0, 61) + 48;
+        a = random.randint(0, 61);
+        print(a)
+        a += 48;
         if(a > 57):
-            a += 8
-        if(a > 90):
             a += 7
-            list.append(chr(a))
-    print(list);
+        if(a > 90):
+            a += 6
+        p += chr(a)
+    print(p);
+    print(d);
 
-password(60)
-print(" ")
-password(20)
-print(" ")
-password(9)
+password(100)
+
+print(chr(48))

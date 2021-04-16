4c3e631c7c896962cad09f3a063bd9ab85e10431
KoreyEnglert <englertk@oregonstate.edu>
2021-04-15 17:17:40 -0700

    fixed bugs

--- password.py
+++ password.py
@@ -1,14 +1,14 @@
 import random
 
 def password(a): #commit 1
-    list [];
+    list = [];
     for x in range(a):
         a = random.randint(0, 61) + 48;
         if(a > 57):
             a += 8
         if(a > 90):
             a += 7
-            list.append(a)
+            list.append(chr(a))
     print(list);
 
 password(60)

70848dd7f0060a1065763248a02dcdab136223ed
KoreyEnglert <englertk@oregonstate.edu>
2021-04-15 17:28:01 -0700

    cleaned it up and added test cases

--- password.py
+++ password.py
@@ -2,10 +2,8 @@ import random
 
 def password(a): #commit 1
     p = "";
-    d = "";
     for x in range(a):
         a = random.randint(0, 61);
-        print(a)
         a += 48;
         if(a > 57):
             a += 7
@@ -13,8 +11,9 @@ def password(a): #commit 1
             a += 6
         p += chr(a)
     print(p);
-    print(d);
 
-password(100)
-
-print(chr(48))
+password(10)
+print(" ")
+password(20)
+print(" ")
+password(5)
